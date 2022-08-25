60dd72e83fa6f656855f416c090a63a7a37b7c22
Parham2002 <parhamm2002@gmail.com>
2022-08-25 10:17:49 +0100

    Clicking mines logs "failed" now

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -2,6 +2,7 @@
 
 var gameBox = document.querySelector(".game-box");
 var code = "";
+var minesList = [];
 
 for (i = 1; i < 201; i++) {
   code = code + "<button class=\"game-box__buttons\"></button>";
@@ -9,21 +10,33 @@ for (i = 1; i < 201; i++) {
 
 gameBox.innerHTML = code;
 var buttons = document.querySelectorAll(".game-box__buttons");
-buttons.forEach(function (button) {
-  var mines = Math.floor(Math.random() * 100 + 1);
+var mineChance = 40;
 
-  if (mines < 35) {
-    button.innerText = "*";
-  }
-});
+var mineField = function mineField(Chance) {
+  buttons.forEach(function (button) {
+    var percentage = Math.floor(Math.random() * 100);
+
+    if (percentage < Chance) {
+      button.innerHTML = "<img class='game-box__mines' src='./Assets/Mine.jpg' width='20px'></img>";
+      minesList.push(button);
+    }
+  });
+};
+
+var minedButtons = document.querySelectorAll(".game-box__mines");
 
 var buttonIsClicked = function buttonIsClicked(button) {
   return button.forEach(function (x) {
     x.addEventListener("click", function () {
-      x.innerText = 1;
+      if (x.innerHTML.includes("game-box__mines")) {
+        console.log("FAILED");
+      } else {
+        x.innerText = 1;
+      }
     });
   });
 };
 
 buttonIsClicked(buttons);
-console.log(Math.floor(Math.random() * 20) + 1);
+mineField(mineChance);
+console.log(minedButtons);
--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const gameBox = document.querySelector(".game-box")
 
 
 let code= ``
+let minesList = [];
 for (i=1; i<201; i++) {
     code = code + `<button class="game-box__buttons"></button>`
 }
@@ -9,19 +10,31 @@ gameBox.innerHTML = code
 const buttons = document.querySelectorAll(".game-box__buttons")
 
 
-buttons.forEach((button) => {
-    let mines = Math.floor(Math.random() * 100 + 1)
-    if (mines < 35) {
-        button.innerText = "*"
-    }
-})
+let mineChance = 40;
+const mineField = (Chance) => {
+    buttons.forEach((button) => {
+        let percentage = Math.floor(Math.random() * 100)
+        if (percentage < Chance) {
+            button.innerHTML = "<img class='game-box__mines' src='./Assets/Mine.jpg' width='20px'></img>"
+            minesList.push(button)
+        }
+    })
+}
+const minedButtons = document.querySelectorAll(".game-box__mines")
+
+
 
 const buttonIsClicked = (button) => button.forEach((x) => {
     x.addEventListener("click", () =>{
+       if (x.innerHTML.includes("game-box__mines")) {
+        console.log("FAILED");
+       } else {
         x.innerText = 1
+       }
     })
 })
 
 buttonIsClicked(buttons)
+mineField(mineChance)
 
-console.log(Math.floor(Math.random() * 20) +1);
+console.log(minedButtons);
