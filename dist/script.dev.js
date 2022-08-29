1a31d44ea9925751744ccb636fb1f3416fa28922
Parham2002 <parhamm2002@gmail.com>
2022-08-29 19:19:15 +0100

    Made it so each empty square checks neighbour squares

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -3,9 +3,11 @@
 var gameBox = document.querySelector(".game-box");
 var code = "";
 var minesList = [];
+var buttonId = [];
 
 for (i = 1; i < 201; i++) {
-  code = code + "<button class=\"game-box__buttons\"></button>";
+  buttonId.push(i);
+  code = code + "<button id=\"".concat(i, "\" class=\"game-box__buttons\">").concat(i, "</button>");
 }
 
 gameBox.innerHTML = code;
@@ -16,27 +18,53 @@ var mineField = function mineField(Chance) {
   buttons.forEach(function (button) {
     var percentage = Math.floor(Math.random() * 100);
 
-    if (percentage < Chance) {
+    if (percentage <= Chance) {
       button.innerHTML = "<img class='game-box__mines' src='./Assets/Mine.jpg' width='20px'></img>";
       minesList.push(button);
     }
   });
 };
 
-var minedButtons = document.querySelectorAll(".game-box__mines");
-
 var buttonIsClicked = function buttonIsClicked(button) {
-  return button.forEach(function (x) {
-    x.addEventListener("click", function () {
-      if (x.innerHTML.includes("game-box__mines")) {
-        console.log("FAILED");
+  return button.forEach(function (box) {
+    box.addEventListener("click", function () {
+      if (box.innerHTML.includes("game-box__mines")) {
+        alert("YOU FAILED");
       } else {
-        x.innerText = 1;
+        box.innerText = nearbyMines(box);
       }
     });
   });
 };
 
+var nearbyMines = function nearbyMines(clickedBox) {
+  var isRightEdge = clickedBox.id % 10 == 0;
+  var isLeftEdge = clickedBox.id % 10 == 1;
+  var numberOfMines = 0; // Right neighbour
+
+  if (!isRightEdge && buttons[clickedBox.id].innerHTML.includes("game-box__mines")) {
+    numberOfMines += 1;
+  } // Left neighbour
+
+
+  if (!isLeftEdge && buttons[clickedBox.id - 2].innerHTML.includes("game-box__mines")) {
+    numberOfMines += 1;
+  } // Top neighbour
+
+
+  if (buttons[clickedBox.id - 11] >= buttons[0] && buttons[clickedBox.id - 11].innerHTML.includes("game-box__mines")) {
+    numberOfMines += 1;
+  } // Bottom neighbour
+
+
+  if (buttons[clickedBox.id - -9] <= buttons[199] && buttons[clickedBox.id - -9].innerHTML.includes("game-box__mines")) {
+    numberOfMines += 1;
+  }
+
+  console.log(clickedBox.id);
+  console.log(buttons[clickedBox.id]);
+  return numberOfMines;
+};
+
 buttonIsClicked(buttons);
 mineField(mineChance);
-console.log(minedButtons);
--- script.js
+++ script.js
@@ -3,38 +3,65 @@ const gameBox = document.querySelector(".game-box")
 
 let code= ``
 let minesList = [];
+let buttonId= [];
 for (i=1; i<201; i++) {
-    code = code + `<button class="game-box__buttons"></button>`
+    buttonId.push(i)
+    code = code + `<button id="${i}" class="game-box__buttons">${i}</button>`
 }
 gameBox.innerHTML = code
 const buttons = document.querySelectorAll(".game-box__buttons")
-
 
 let mineChance = 40;
 const mineField = (Chance) => {
     buttons.forEach((button) => {
         let percentage = Math.floor(Math.random() * 100)
-        if (percentage < Chance) {
+        if (percentage <= Chance) {
             button.innerHTML = "<img class='game-box__mines' src='./Assets/Mine.jpg' width='20px'></img>"
             minesList.push(button)
         }
     })
 }
-const minedButtons = document.querySelectorAll(".game-box__mines")
 
 
 
-const buttonIsClicked = (button) => button.forEach((x) => {
-    x.addEventListener("click", () =>{
-       if (x.innerHTML.includes("game-box__mines")) {
-        console.log("FAILED");
+const buttonIsClicked = (button) => button.forEach((box) => {
+    box.addEventListener("click", () =>{
+       if (box.innerHTML.includes("game-box__mines")) {
+        alert("YOU FAILED")
        } else {
-        x.innerText = 1
+        box.innerText = nearbyMines(box)
        }
     })
 })
 
+
+const nearbyMines = (clickedBox) => {
+    const isRightEdge = (clickedBox.id % 10 == 0)
+    const isLeftEdge = (clickedBox.id % 10 == 1)
+    let numberOfMines = 0;
+    // Right neighbour
+    if (!isRightEdge && buttons[clickedBox.id].innerHTML.includes("game-box__mines")) {
+        numberOfMines += 1
+    }
+    // Left neighbour
+    if (!isLeftEdge && buttons[clickedBox.id-2].innerHTML.includes("game-box__mines")) {
+        numberOfMines += 1
+    }
+    // Top neighbour
+    if (buttons[clickedBox.id-11] >= buttons[0] && buttons[clickedBox.id-11].innerHTML.includes("game-box__mines")) {
+        numberOfMines += 1
+    }
+    // Bottom neighbour
+    if (buttons[clickedBox.id- -9] <= buttons[199] && buttons[clickedBox.id- -9].innerHTML.includes("game-box__mines")) {
+        numberOfMines += 1
+    }
+    console.log(clickedBox.id);
+    console.log(buttons[clickedBox.id]);
+    return numberOfMines
+
+    
+
+
+}
 buttonIsClicked(buttons)
 mineField(mineChance)
-
-console.log(minedButtons);
